90a3c3d8f0f61a6e2854d5c5cbe7f5435bc4caed
DoctorPok <pokdoctor@gmail.com>
2024-11-08 17:25:26 +0100

    Feat -> skills: add system

--- src/main/java/Class/Skill/Skill.java
+++ src/main/java/Class/Skill/Skill.java
@@ -2,14 +2,28 @@ package Class.Skill;
 
 public class Skill {
     private String name;
+    private int level;
 
     //Constructor
     public Skill(String name){
         this.name = name;
+        this.level = 0;
     }
 
     //Getter
     public String getName(){
         return this.name;
+    }
+
+    public int getLevel(){
+        return this.level;
+    }
+
+    public void levelUp(){
+        this.level++;
+    }
+
+    public void levelDown(){
+        this.level--;
     }
 }
