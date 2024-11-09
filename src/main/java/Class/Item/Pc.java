66907a1501efc1fecba5c7b4fee508173e2f54e8
DoctorPok <pokdoctor@gmail.com>
2024-11-09 19:57:07 +0100

    Feat -> pc item: add differents actions

--- src/main/java/Class/Item/Pc.java
+++ src/main/java/Class/Item/Pc.java
@@ -1,5 +1,8 @@
 package Class.Item;
 
+import Class.Character.Player;
+import Class.Skill.Skill;
+
 public class Pc extends Item {
     private Enum<PcTypeAction> typeAction = PcTypeAction.WORK;
 
@@ -16,7 +19,39 @@ public class Pc extends Item {
         System.out.println("Displaying menu...");
     }
 
-    public void doAction(int nbSleep) {
-        System.out.println("Doing action...");
+    private boolean work(Player player, int time, String module) {
+        if (module == null)
+            return false;
+
+        if (time > 2) {
+            if (player.getSkill(module) != null) {
+                player.getSkill(module).setLevel(player.getSkill(module).getLevel() + 1);
+            } else {
+                player.getSkills().add(new Skill(module));
+                player.getSkill(module).setLevel(player.getSkill(module).getLevel() + 1);
+            }
+        }
+
+        player.addTime(time);
+        return true;
+    }
+
+    private boolean play(Player player, int time) {
+        player.addTime(time);
+        return true;
+    }
+
+    private boolean search(Player player, int time) {
+        player.addTimeOfSearch(time);
+        return true;
+    }
+
+    public boolean doAction(Player player, Enum<PcTypeAction> action, int time, String module) {
+        return switch (action) {
+            case PcTypeAction.WORK -> work(player, time, module);
+            case PcTypeAction.PLAY -> play(player, time);
+            case PcTypeAction.SEARCH -> search(player, time);
+            default -> false;
+        };
     }
 }
