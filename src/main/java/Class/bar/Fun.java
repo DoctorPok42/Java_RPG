85934b157e47d30fc473a45aeaa64dd6a5f2c7b8
DoctorPok <pokdoctor@gmail.com>
2024-11-15 11:29:56 +0100

    Update Fun.java

--- src/main/java/Class/bar/Fun.java
+++ src/main/java/Class/bar/Fun.java
@@ -3,7 +3,7 @@ package Class.bar;
 import javafx.scene.image.ImageView;
 
 public class Fun extends bar {
-    public Fun(String name, int maxCapacity, int currentCapacity) {
+    public Fun(String name) {
         super(name, new ImageView("file:assets/bar/fun.png"));
     }
 }
