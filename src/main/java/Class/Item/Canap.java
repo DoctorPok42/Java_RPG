34b1232a3866d8326a5388c51455e84d9fb56bf0
DoctorPok <pokdoctor@gmail.com>
2024-11-22 15:46:46 +0100

    Update Canap.java

--- src/main/java/Class/Item/Canap.java
+++ src/main/java/Class/Item/Canap.java
@@ -1,10 +1,8 @@
 package Class.Item;
 
 import Class.Character.Player;
-import javafx.scene.image.Image;
 
 import java.util.List;
-import java.util.ArrayList;
 import javafx.scene.image.ImageView;
 
 public class Canap extends Item {
