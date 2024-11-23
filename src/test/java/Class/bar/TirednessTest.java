38f7ccf520e6c62ebe751718856b866c1df17b74
DoctorPok <pokdoctor@gmail.com>
2024-11-23 19:22:24 +0100

    Update TirednessTest.java

--- src/test/java/Class/bar/TirednessTest.java
+++ src/test/java/Class/bar/TirednessTest.java
@@ -1,11 +1,19 @@
 package Class.bar;
 
+import javafx.application.Platform;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class TirednessTest {
+
+    @BeforeAll
+    public static void initJavaFX() {
+        // Initialise JavaFX
+        Platform.startup(() -> {});
+    }
 
     @Test
     public void testGetNameWithValidName() {
