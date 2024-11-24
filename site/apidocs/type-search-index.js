b36817a067359f1b461982a9c2bfa4d3b29a27fd
DoctorPok <pokdoctor@gmail.com>
2024-11-24 19:43:19 +0100

    Feat -> add docs and build

---
+++ site/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"Game","l":"Game"},{"p":"com.example.demo1","l":"HelloApplication"},{"p":"com.example.demo1","l":"HelloController"}];updateSearchResults();
